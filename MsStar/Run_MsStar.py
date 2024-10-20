29a2d2f49b6950de1b5f81695f78771a3f378402
yehonatan280198 <kidushim@post.bgu.ac.il>
2024-10-20 20:39:36 +0300

    update

--- MsStar/Run_MsStar.py
+++ MsStar/Run_MsStar.py
@@ -28,19 +28,20 @@ def find_collision(Vk, Vl):
 
     # Vertex conflicts
     location_counts = {}
-    for i, (agent_loc, _) in enumerate(Vl):
+    for agent, (agent_loc, _) in enumerate(Vl):
         if agent_loc in location_counts:
-            agent_conflict.update([i, location_counts[agent_loc]])
+            agent_conflict.update([agent, location_counts[agent_loc]])
         else:
-            location_counts[agent_loc] = i
+            location_counts[agent_loc] = agent
 
     # Edge conflicts
-    agent_loc_dict = {loc: idx for idx, (loc, _) in enumerate(Vl)}
-    for i, (agent_loc, _) in enumerate(Vl):
-        if Vk[i] != agent_loc and Vk[i] in agent_loc_dict:
-            index = agent_loc_dict[Vk[i]]
-            if agent_loc == Vk[index]:
-                agent_conflict.update([i, index])
+    agent_curr_loc_dict = {agent_loc: agent for agent, (agent_loc, _) in enumerate(Vl)}
+    for agent, (agent_curr_loc, _) in enumerate(Vl):
+        previous_loc = Vk[agent]
+        if previous_loc != agent_curr_loc and previous_loc in agent_curr_loc_dict:
+            other_agent = agent_curr_loc_dict[previous_loc]
+            if agent_curr_loc == Vk[other_agent]:
+                agent_conflict.update([agent, other_agent])
 
     return agent_conflict
 
@@ -56,12 +57,14 @@ class MsStar:
 
         # Initialize OPEN with so = (vo, 0^M)
         S0 = State(initial_locations, [0] * len(taskLocs))
-        h_val, task_allocation = self.calc_heuristic_value2(S0)
+        h_val, task_allocation = self.Heuristics_for_makespan_and_task_order_allocation(S0)
         self.OPEN.put((h_val + 0, S0))
 
+        print(task_allocation)
         self.compute_individual_optimal_policy(task_allocation, initial_locations)
 
     def run(self):
+        print(self.optimal_policy)
         # While OPEN not empty do
         while not self.OPEN.empty():
 
@@ -89,7 +92,7 @@ class MsStar:
 
                 # f(sl) ← g(sl) + h(sl)
                 Sl.g = Sk.g + 1
-                F_val = Sl.g + self.calc_heuristic_value2(Sl)[0]
+                F_val = Sl.g + self.Heuristics_for_makespan_and_task_order_allocation(Sl)[0]
 
                 dominated_state = Check_who_is_dominant(Sl, self.Alpha[Sl.Vk])
 
@@ -99,7 +102,7 @@ class MsStar:
                     Sl.parent = Sk  # parent(sl) ← sk
                     self.Alpha[Sl.Vk].add(Sl)
 
-                # else (Notes: sl is dominated by states in α(vl))
+                # else (sl is dominated by states in α(vl))
                 else:
 
                     # for all s'l ∈ A do
@@ -121,7 +124,7 @@ class MsStar:
             min_longest_paths.append(min(distances, default=0))
 
         heuristic_makespan = max(min_longest_paths)
-        return heuristic_makespan
+        return heuristic_makespan, []
 
     def validateMove(self, loc):
         return not (loc // self.num_cols >= self.num_rows or loc % self.num_cols >= self.num_cols)
@@ -139,10 +142,15 @@ class MsStar:
                 action = combination[i]
 
                 if i not in Sk.Ic:
-                    next_optimal_move = self.optimal_policy[i][min(self.optimal_policy[i].index((agent_loc, agent_direction)) + 1,len(self.optimal_policy[i])-1)]
+                    next_optimal_move = self.optimal_policy[i][
+                        min(self.optimal_policy[i].index((agent_loc, agent_direction)) + 1,
+                            len(self.optimal_policy[i]) - 1)]
                     vi.append(next_optimal_move)
                     if next_optimal_move[0] in self.taskLocs:
                         ai[self.taskLocs.index(next_optimal_move[0])] = 1
+
+                # if 1 == 0:
+                #     pass
 
                 elif action == 0:
                     candidates = [agent_loc + 1, agent_loc + self.num_cols, agent_loc - 1, agent_loc - self.num_cols]
@@ -177,86 +185,106 @@ class MsStar:
         if not I.issubset(s.Ic):
             s.Ic.update(I)
             if s not in {item[1] for item in self.OPEN.queue}:
-                F_val = s.g + self.calc_heuristic_value(s)
+                F_val = s.g + self.Heuristics_for_makespan_and_task_order_allocation(s)[0]
                 self.OPEN.put((F_val, s))
             for Sk in s.back_set:
                 self.BackProp(Sk, s.Ic)
 
-    def calc_heuristic_value2(self, s):
-        makespan = []
-        goal_permutations = list(permutations(self.taskLocs))
+    def Heuristics_for_makespan_and_task_order_allocation(self, s):
+        makespan_per_combination = []
+        uncollected_tasks = [task for index, task in enumerate(self.taskLocs) if s.Ak[index] == 0]
 
-        for perm in goal_permutations:
-            agent_allocations = product(range(0,len(s.Vk)), repeat=len(perm))
+        goals_permutations = list(permutations(uncollected_tasks))
+        for goals_perm in goals_permutations:
+            agents_allocations = product(range(0, len(s.Vk)), repeat=len(goals_perm))
 
-            for allocation in agent_allocations:
-                combination = list(zip(perm, allocation))
-                goal_locations = [[loc] for loc, _ in s.Vk]
+            for agents_alloc in agents_allocations:
+                combination = list(zip(goals_perm, agents_alloc))
+                goal_locations = [[[loc, direction]] for (loc, direction) in s.Vk]
 
                 for goal, agent in combination:
-                    goal_locations[agent].append(goal)
+                    goal_locations[agent].append([goal, -1])
 
-                time_to_finish = []
+                finish_time_of_each_agent = []
 
                 for goals in goal_locations:
-                    time = 0
+                    total_time = 0
                     for i in range(len(goals) - 1):
-                        time = (time + abs(goals[i] // self.num_cols - goals[i + 1] // self.num_cols) +
-                                abs(goals[i] % self.num_cols - goals[i + 1] % self.num_cols))
+                        time, final_direction = self.compute_minimal_time_to_reach_goal_and_final_direction(goals[i], goals[i + 1])
+                        total_time = total_time + time
+                        goals[i + 1][1] = final_direction
 
-                    if len(goals) != 1:
-                        time_to_finish.append((time, goal_locations))
+                    finish_time_of_each_agent.append((total_time, [[goal[0] for goal in goals] for goals in goal_locations]))
 
-                makespan.append(max(time_to_finish))
+                makespan_per_combination.append(max(finish_time_of_each_agent))
 
-        return min(makespan)
+        return min(makespan_per_combination)
 
     def compute_individual_optimal_policy(self, task_allocation, initial_location):
         for agent, goals_per_agent in enumerate(task_allocation):
             self.optimal_policy[agent] = [initial_location[agent]]
 
-            if len(goals_per_agent) != 1:
-                for i in range(len(goals_per_agent) - 1):
-                    curr_loc = list(self.optimal_policy[agent][-1])
-                    next_goal = goals_per_agent[i + 1]
+            for i in range(len(goals_per_agent) - 1):
+                curr_loc, curr_direct = list(self.optimal_policy[agent][-1])
+                next_goal = goals_per_agent[i + 1]
 
-                    up_or_down = -1
-                    left_or_right = -1
+                curr_row = curr_loc // self.num_cols
+                curr_col = curr_loc % self.num_cols
+                next_goal_row = next_goal // self.num_cols
+                next_goal_col = next_goal % self.num_cols
 
-                    if goals_per_agent[i] // self.num_cols > next_goal // self.num_cols:
-                        up_or_down = 3
-                    if goals_per_agent[i] // self.num_cols < next_goal // self.num_cols:
-                        up_or_down = 1
-                    if goals_per_agent[i] % self.num_cols > next_goal % self.num_cols:
-                        left_or_right = 2
-                    if goals_per_agent[i] % self.num_cols < next_goal % self.num_cols:
-                        left_or_right = 0
+                up_or_down = 3 if curr_row > next_goal_row else (1 if curr_row < next_goal_row else None)
+                left_or_right = 2 if curr_col > next_goal_col else (0 if curr_col < next_goal_col else None)
 
-                    while curr_loc[0] != next_goal:
-                        if curr_loc[1] == up_or_down == 1:
-                            while (curr_loc[0] + self.num_cols) // self.num_cols <= (goals_per_agent[i + 1]) // self.num_cols:
-                                curr_loc[0] += self.num_cols
-                                self.optimal_policy[agent].append(tuple(curr_loc))
+                while curr_loc != next_goal:
 
-                        elif curr_loc[1] == up_or_down == 3:
-                            while (curr_loc[0] - self.num_cols) // self.num_cols >= (goals_per_agent[i + 1]) // self.num_cols:
-                                curr_loc[0] -= self.num_cols
-                                self.optimal_policy[agent].append(tuple(curr_loc))
+                    if curr_direct == up_or_down == 1:
+                        while (curr_loc + self.num_cols) // self.num_cols <= next_goal_row:
+                            curr_loc += self.num_cols
+                            self.optimal_policy[agent].append(tuple((curr_loc, curr_direct)))
 
-                        elif curr_loc[1] == left_or_right == 0:
-                            while (curr_loc[0] + 1) % self.num_cols <= goals_per_agent[i + 1] % self.num_cols:
-                                curr_loc[0] += 1
-                                self.optimal_policy[agent].append(tuple(curr_loc))
+                    elif curr_direct == up_or_down == 3:
+                        while (curr_loc - self.num_cols) // self.num_cols >= next_goal_row:
+                            curr_loc -= self.num_cols
+                            self.optimal_policy[agent].append(tuple((curr_loc, curr_direct)))
 
-                        elif curr_loc[1] == left_or_right == 2:
-                            while (curr_loc[0] - 1) % self.num_cols >= goals_per_agent[i + 1] % self.num_cols:
-                                curr_loc[0] -= 1
-                                self.optimal_policy[agent].append(tuple(curr_loc))
+                    elif curr_direct == left_or_right == 0:
+                        while (curr_loc + 1) % self.num_cols <= next_goal_col:
+                            curr_loc += 1
+                            self.optimal_policy[agent].append(tuple((curr_loc, curr_direct)))
 
-                        if curr_loc[0] != next_goal:
-                            if (curr_loc[1] + 1) % 4 == up_or_down or (curr_loc[1] + 1) % 4 == left_or_right:
-                                curr_loc[1] = (curr_loc[1] + 1) % 4
-                            elif (curr_loc[1] - 1) % 4 == up_or_down or (curr_loc[1] - 1) % 4 == left_or_right:
-                                curr_loc[1] = (curr_loc[1] - 1) % 4
-                            self.optimal_policy[agent].append(tuple(curr_loc))
+                    elif curr_direct == left_or_right == 2:
+                        while (curr_loc - 1) % self.num_cols >= next_goal_col:
+                            curr_loc -= 1
+                            self.optimal_policy[agent].append(tuple((curr_loc, curr_direct)))
 
+                    if curr_loc != next_goal:
+                        if (curr_direct + 1) % 4 == up_or_down or (curr_direct + 1) % 4 == left_or_right:
+                            curr_direct = (curr_direct + 1) % 4
+                        elif (curr_direct - 1) % 4 == up_or_down or (curr_direct - 1) % 4 == left_or_right:
+                            curr_direct = (curr_direct - 1) % 4
+                        self.optimal_policy[agent].append(tuple((curr_loc, curr_direct)))
+
+    def compute_minimal_time_to_reach_goal_and_final_direction(self, curr, goal):
+        curr_row = curr[0] // self.num_cols
+        curr_col = curr[0] % self.num_cols
+        goal_row = goal[0] // self.num_cols
+        goal_col = goal[0] % self.num_cols
+
+        time = abs(curr_row - goal_row) + abs(curr_col - goal_col)
+
+        up_or_down = 3 if curr_row > goal_row else (1 if curr_row < goal_row else None)
+        left_or_right = 2 if curr_col > goal_col else (0 if curr_col < goal_col else None)
+
+        direction = -1
+
+        if curr[1] == up_or_down or curr[1] == left_or_right:
+            if curr_row != goal_row and curr_col != goal_col:
+                time += 1
+            direction = left_or_right if curr[1] == up_or_down else up_or_down
+
+        else:
+            time += 2
+            direction = left_or_right if curr[1] in [1, 3] else up_or_down
+
+        return time, direction
