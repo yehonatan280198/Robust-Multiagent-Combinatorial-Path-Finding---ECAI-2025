4e004573bef239d4bfc497d96c73552e4af2deb5
yehonatan280198 <kidushim@post.bgu.ac.il>
2025-02-04 20:47:13 +0200

    4/2

--- pRobustCbss/TestAlgorithm.py
+++ pRobustCbss/TestAlgorithm.py
@@ -1,36 +1,16 @@
-import random
 import time
 import csv
+import ast
 
-from collections import defaultdict
 from multiprocessing import Process, Queue
 from pRobustCbss.Run_pRobustCbss import pRobustCbss
 
-columns = ["Map", "No collision prob", "Delay prob", "Number of agent", "Number of goals", "Number of successes",
-           "Avg time", "Avg calls to TSP", "Avg calls to BFS"]
 with open("output.csv", mode="w", newline="", encoding="utf-8") as file:
+    columns = ["Map", "No collision prob", "Delay prob", "Number of agent", "Number of goals", "Number of successes",
+               "AVG elapsed time", "AVG elapsed time (successes only)", "Avg calls to TSP", "Avg calls to BFS"]
     writer = csv.DictWriter(file, fieldnames=columns)
     writer.writeheader()
 
-config_log_file = "config_log.txt"
-with open(config_log_file, mode="w", encoding="utf-8") as log_file:
-    log_file.write("Configuration Log\n")
-    log_file.write("=" * 50 + "\n\n")
-
-
-def log_configuration_to_text(map_name, no_collision_prob, delaysProb, numAgents, numGoals, AgentsPositions,
-                              GoalsLocations, iteration):
-    """Log the current configuration to the text file."""
-    with open(config_log_file, mode="a", encoding="utf-8") as log_file:
-        log_file.write(f"Iteration: {iteration}\n")
-        log_file.write(f"Map: {map_name}\n")
-        log_file.write(f"No Collision Probability: {no_collision_prob}\n")
-        log_file.write(f"Delays Probability: {delaysProb}\n")
-        log_file.write(f"Number of Agents: {numAgents}\n")
-        log_file.write(f"Number of Goals: {numGoals}\n")
-        log_file.write(f"Agents Positions: {AgentsPositions}\n")
-        log_file.write(f"Goals Locations: {GoalsLocations}\n")
-        log_file.write("-" * 50 + "\n")
 
 def create_map(name):
     file_path = f"/home/yonikid/Desktop/SimulatorAgents/Start-Kit-main/example_problems/OurResearch.domain/{name}"
@@ -42,16 +22,17 @@ def create_map(name):
     return {"Rows": 32, "Cols": 32, "Map": currMap}
 
 
-def create_positions_for_agents_And_Locs_For_Goals(NumAgents, NumGoals, MapAndDim):
-    zero_indices = [i for i, value in enumerate(MapAndDim["Map"]) if value == 0]
-    chosen_indices_for_agents = random.sample(zero_indices, NumAgents)
-    position_for_agents = [(num, random.randint(0, 3)) for num in chosen_indices_for_agents]
+def read_locs_from_file(mapName, num_of_iter, num_of_agent, num_of_goals):
+    file_agents_name = f"Agent_Goal_locations_files/{mapName.split('-')[0].upper()}_Map_Agent_Locs_iteration_{num_of_iter}.txt"
+    file_goals_name = f"Agent_Goal_locations_files/{mapName.split('-')[0].upper()}_Map_Goal_Locs_iteration_{num_of_iter}.txt"
 
-    chosen_set = set(chosen_indices_for_agents)
-    remaining_indices_for_goals = [idx for idx in zero_indices if idx not in chosen_set]
-    location_for_agents = random.sample(remaining_indices_for_goals, NumGoals)
+    with open(file_agents_name, "r") as f:
+        Agents_Positions = [ast.literal_eval(line.strip()) for _, line in zip(range(num_of_agent), f)]
 
-    return position_for_agents, location_for_agents
+    with open(file_goals_name, "r") as f:
+        Goals_Locations = [ast.literal_eval(line.strip()) for _, line in zip(range(num_of_goals), f)]
+
+    return Agents_Positions, Goals_Locations
 
 
 def run_pRobustCbss(queue, Positions, GoalLocations, No_collision_prob, DelaysProbDict, dict_of_map_and_dim,
@@ -60,17 +41,18 @@ def run_pRobustCbss(queue, Positions, GoalLocations, No_collision_prob, DelaysPr
     queue.put(p.Solution)
 
 
-# mapAndDim = create_map()
-mapsList = ["empty-32-32.map", "random-32-32-20.map", "maze-32-32-2.map", "room-32-32-4.map"]  # 4
-num_of_agentsList = [3, 5, 10, 15, 20, 25]  # 6
-num_of_goalsList = [6, 10, 20, 30, 40, 50]  # 6
-no_collision_probList = [0.6, 0.7, 0.8]  # 3
-delays_probList = [0.05, 0.3]  # 2
-iterations = 10  # 10
+mapsList = ["empty-32-32.map", "random-32-32-20.map", "maze-32-32-2.map", "room-32-32-4.map"]
+# num_of_agentsList = [3, 5, 10, 15, 20, 25]
+# num_of_goalsList = [6, 10, 20, 30, 40, 50]
+# no_collision_probList = [0.6, 0.7, 0.8]
+# delays_probList = [0.05, 0.3]
+num_of_agentsList = [5, 15, 25]
+num_of_goalsList = [10, 30, 50]
+no_collision_probList = [0.6]
+delays_probList = [0.05]
+iterations = 10
 verifyAlpha = 0.05
 max_time = 60
-
-dict_of_successes = defaultdict(lambda: [0, 0, 0, 0])
 
 for map_name in mapsList:
     mapAndDim = create_map(map_name)
@@ -80,25 +62,21 @@ for map_name in mapsList:
                 delaysProbDict = {i: delaysProb for i in range(numAgents)}
                 for numGoals in num_of_goalsList:
 
-                    for iteration in range(iterations):
-                        AgentsPositions, GoalsLocations = create_positions_for_agents_And_Locs_For_Goals(numAgents,
-                                                                                                         numGoals,
-                                                                                                         mapAndDim)
+                    statisticConfig = {"Success": 0, "ElapsedTimeOfSuccess": 0, "CallToBFS": 0, "CallToTSP": 0,
+                                       "TotalTime": 0}
 
-                        log_configuration_to_text(
-                            map_name, no_collision_prob, delaysProb, numAgents, numGoals, AgentsPositions, GoalsLocations, iteration
-                        )
+                    for iteration in range(iterations):
+                        AgentsPositions, GoalsLocations = read_locs_from_file(map_name, iteration, numAgents, numGoals)
 
                         print("AgentsPositions:", AgentsPositions)
                         print("GoalsLocations:", GoalsLocations)
-                        successful_runs = True
 
                         queue = Queue()
-                        start_time = time.time()
                         process = Process(target=run_pRobustCbss, args=(
-                        queue, AgentsPositions, GoalsLocations, no_collision_prob, delaysProbDict, mapAndDim,
-                        verifyAlpha))
+                            queue, AgentsPositions, GoalsLocations, no_collision_prob, delaysProbDict, mapAndDim,
+                            verifyAlpha))
                         process.start()
+                        start_time = time.time()
                         process.join(timeout=max_time)
 
                         if process.is_alive():
@@ -106,45 +84,30 @@ for map_name in mapsList:
                             process.join()
                             print(f"Skipped due to timeout after {max_time} seconds.")
                             print("--------------------------------------------------------------------------")
-                            successful_runs = False
+                            statisticConfig["TotalTime"] += max_time
                             continue
 
-                        end_time = time.time()
-                        elapsed_time = round(end_time - start_time, 2)
+                        elapsed_time = round(time.time() - start_time, 2)
 
                         # If the process completed in time, retrieve the solution
                         solution = queue.get()
-                        print("Solution:", solution)
+                        print("Solution:", solution[0])
                         print("--------------------------------------------------------------------------")
 
-                        if successful_runs:
-                            dict_of_successes[(map_name, no_collision_prob, delaysProb, numAgents, numGoals)][0] += 1
-                            dict_of_successes[(map_name, no_collision_prob, delaysProb, numAgents, numGoals)][
-                                1] += elapsed_time
-                            dict_of_successes[(map_name, no_collision_prob, delaysProb, numAgents, numGoals)][2] += \
-                            solution[1]  # TSP
-                            dict_of_successes[(map_name, no_collision_prob, delaysProb, numAgents, numGoals)][3] += \
-                            solution[2]  # BFS
+                        statisticConfig["Success"] += 1
+                        statisticConfig["ElapsedTimeOfSuccess"] += elapsed_time
+                        statisticConfig["CallToTSP"] += solution[1]
+                        statisticConfig["CallToBFS"] += solution[2]
+                        statisticConfig["TotalTime"] += elapsed_time
 
-                    count_success = dict_of_successes[(map_name, no_collision_prob, delaysProb, numAgents, numGoals)][0]
-                    if count_success > 0:
-                        record = [
-                            map_name,
-                            no_collision_prob,
-                            delaysProb,
-                            numAgents,
-                            numGoals,
-                            count_success,
-                            round(dict_of_successes[(map_name, no_collision_prob, delaysProb, numAgents, numGoals)][
-                                      1] / count_success, 3),
-                            round(dict_of_successes[(map_name, no_collision_prob, delaysProb, numAgents, numGoals)][
-                                      2] / count_success, 3),
-                            round(dict_of_successes[(map_name, no_collision_prob, delaysProb, numAgents, numGoals)][
-                                      3] / count_success, 3),
-                        ]
-                        with open("output.csv", mode="a", newline="", encoding="utf-8") as file:
-                            writer = csv.writer(file)
-                            writer.writerow(record)
+                    num_of_success = statisticConfig["Success"] if statisticConfig["Success"] > 0 else 1
+                    record = [map_name, no_collision_prob, delaysProb, numAgents, numGoals, statisticConfig["Success"],
+                              round(statisticConfig["TotalTime"] / 10, 3),
+                              round(statisticConfig["ElapsedTimeOfSuccess"] / num_of_success, 3),
+                              round(statisticConfig["CallToTSP"] / num_of_success, 3),
+                              round(statisticConfig["CallToBFS"] / num_of_success, 3),
+                              ]
 
-                    else:
-                        break
+                    with open("output.csv", mode="a", newline="", encoding="utf-8") as file:
+                        writer = csv.writer(file)
+                        writer.writerow(record)
